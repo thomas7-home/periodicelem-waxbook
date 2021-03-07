5f5142248a1c2a4a85bce3299e596d2cdb34cc06
sebrock <156824+sebrock@users.noreply.github.com>
2021-03-07 19:25:02 +0100

    Replace BrowserRouter with Hashrouter
    this and only this

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Album from "./routes/Album";
 import Suggestion from "./routes/Suggestion";
 import "./styles.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
